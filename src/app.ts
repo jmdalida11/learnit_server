8a06760de8ce94a0de90c0cc1f59df685b87e8c1
John Mark Dalida <jmdalida11@gmail.com>
2026-03-14 12:12:12 +0800

    Refactor app.ts and datasource.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express } from "express";
 import userRouter from "./routes/user.js";
 import noteRouter from "./routes/note.js";
 import authRouter from "./routes/auth.js";
@@ -9,102 +9,138 @@ import cors from "cors";
 import csurf from "csurf";
 
 import "dotenv/config";
-import { AppDataSource, initializeDataSource } from "./datasource.js";
 import { Session } from "./entities/Session.js";
 import { TypeormStore } from "connect-typeorm";
+import { DataSource } from "typeorm";
+import { createDataSourceInstance } from "datasource.js";
 
 const PORT = process.env["PORT"] || 3000;
 const CORS = process.env["CORS"];
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  session({
-    name: "sessionId",
-    secret: process.env["SESSION_SECRET"] || "supersecret123",
-    resave: false,
-    saveUninitialized: false,
-    rolling: true,
-    cookie: {
-      httpOnly: true, // prevent JS access
-      secure: false, // set true in production (requires HTTPS)
-      maxAge: 1000 * 60 * 60, // 1 hour
-      sameSite: "lax",
-    },
-    store: new TypeormStore({
-      cleanupLimit: 10,
-      ttl: 3600, // seconds
-    }).connect(AppDataSource.getRepository(Session)),
-  }),
-);
-app.use(
-  cors({
-    origin: CORS, // your frontend URL
-    // origin: "http://localhost:5173",
-    credentials: true, // allow cookies
-  }),
-);
-
-export function conditionalCsrf(
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) {
-  const csrfProtection = csurf({ cookie: false });
-  const csrfExcluded = ["/auth/login", "/user"];
-  if (csrfExcluded.includes(req.path)) return next();
-  return csrfProtection(req, res, next);
-}
-app.use(conditionalCsrf);
-
 interface CsrfError extends Error {
   code: string;
 }
-app.use(
-  (
-    err: CsrfError,
-    _req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
-    if (err?.code === "EBADCSRFTOKEN") {
-      res.status(403).json({ message: "Invalid CSRF token" });
-      return;
-    }
-    next(err);
-  },
-);
 
 interface SyntaxErrorWithStatus extends SyntaxError {
   status?: number;
   body?: unknown;
 }
-app.use(
-  (
-    err: SyntaxErrorWithStatus,
-    _req: express.Request,
-    res: express.Response,
-    _next: express.NextFunction,
-  ) => {
-    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-      res.status(400).send({ message: "Invalid JSON payload" });
-      return;
+
+class LearnitApp {
+  private readonly app: Express;
+  private readonly dataSource: DataSource;
+
+  constructor(app: Express, dataSource: DataSource) {
+    this.app = app;
+    this.dataSource = dataSource;
+    this.init();
+  }
+
+  private init(): void {
+    this.initMiddlewares();
+    this.initRouters();
+  }
+
+  private initMiddlewares(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+    this.app.use(
+      session({
+        name: "sessionId",
+        secret: process.env["SESSION_SECRET"] || "supersecret123",
+        resave: false,
+        saveUninitialized: false,
+        rolling: true,
+        cookie: {
+          httpOnly: true, // prevent JS access
+          secure: false, // set true in production (requires HTTPS)
+          maxAge: 1000 * 60 * 60, // 1 hour
+          sameSite: "lax",
+        },
+        store: new TypeormStore({
+          cleanupLimit: 10,
+          ttl: 3600, // seconds
+        }).connect(this.dataSource.getRepository(Session)),
+      }),
+    );
+    this.app.use(
+      cors({
+        origin: CORS, // your frontend URL
+        // origin: "http://localhost:5173",
+        credentials: true, // allow cookies
+      }),
+    );
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+      ) => {
+        const csrfProtection = csurf({ cookie: false });
+        const csrfExcluded = ["/auth/login", "/user"];
+        if (csrfExcluded.includes(req.path)) return next();
+        return csrfProtection(req, res, next);
+      },
+    );
+    this.app.use(
+      (
+        err: CsrfError,
+        _req: express.Request,
+        res: express.Response,
+        next: express.NextFunction,
+      ) => {
+        if (err?.code === "EBADCSRFTOKEN") {
+          res.status(403).json({ message: "Invalid CSRF token" });
+          return;
+        }
+        next(err);
+      },
+    );
+    this.app.use(
+      (
+        err: SyntaxErrorWithStatus,
+        _req: express.Request,
+        res: express.Response,
+        _next: express.NextFunction,
+      ) => {
+        if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+          res.status(400).send({ message: "Invalid JSON payload" });
+          return;
+        }
+        console.error(err);
+        res.status(500).send({ message: "Something went wrong!" });
+      },
+    );
+  }
+
+  private initRouters(): void {
+    this.app.use("/user", userRouter);
+    this.app.use("/note", noteRouter);
+    this.app.use("/auth", authRouter);
+    this.app.use("/quiz", quizRouter);
+  }
+
+  public async initializeDataSource() {
+    try {
+      await this.dataSource.initialize();
+      console.log("Data Source has been initialized!");
+    } catch (error) {
+      console.log(error);
     }
-    console.error(err);
-    res.status(500).send({ message: "Something went wrong!" });
-  },
-);
+  }
 
-app.use("/user", userRouter);
-app.use("/note", noteRouter);
-app.use("/auth", authRouter);
-app.use("/quiz", quizRouter);
+  public run(): void {
+    this.app.listen(PORT, () => {
+      console.log(`Server is running on PORT = ${PORT}`);
+    });
+  }
+}
 
-initializeDataSource();
+const main = async () => {
+  const app = new LearnitApp(express(), createDataSourceInstance());
+  await app.initializeDataSource();
+  app.run();
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+main();
--- src/datasource.ts
+++ src/datasource.ts
@@ -7,24 +7,17 @@ import { StudyPlan } from "./entities/StudyPlan.js";
 import { Notification } from "./entities/Notification.js";
 import { Session } from "./entities/Session.js";
 
-export const AppDataSource = new DataSource({
-  type: "sqlite",
-  // host: process.env["DB_HOST"] || "localhost",
-  // port: 5432,
-  // username: process.env["DB_USER"] || "postgres",
-  // password: process.env["DB_PASSWORD"] || "password",
-  database: process.env["DB_NAME"] || "learnit.db",
-  logging: false,
-  entities: [User, Note, Quiz, Question, StudyPlan, Notification, Session],
-  synchronize: false,
-  migrations: ["src/migrations/*.ts"],
-});
-
-export const initializeDataSource = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log("Data Source has been initialized!");
-  } catch (error) {
-    console.log(error);
-  }
+export const createDataSourceInstance = () => {
+  return new DataSource({
+    type: "sqlite",
+    // host: process.env["DB_HOST"] || "localhost",
+    // port: 5432,
+    // username: process.env["DB_USER"] || "postgres",
+    // password: process.env["DB_PASSWORD"] || "password",
+    database: process.env["DB_NAME"] || "learnit.db",
+    logging: false,
+    entities: [User, Note, Quiz, Question, StudyPlan, Notification, Session],
+    synchronize: false,
+    migrations: ["src/migrations/*.ts"],
+  });
 };
